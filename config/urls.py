4606756df16a7ca3e53510f0a98646262e2bca94
Maksim Yarotskiy <maksimyarotskiy08@gmail.com>
2024-10-19 20:53:13 +0300

    urls работы

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,9 @@
-
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('films.urls')),  # здесь подключается urls для приложения films(пути писать там !!!!)
 ]
+
+# !!! здесь писать listview НЕЛЬЗЯ , перейди в films/urls.py!!!
