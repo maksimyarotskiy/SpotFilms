0acf0a057d9e9a72e2209af7102e92434d3f4df9
Maksim Yarotskiy <maksimyarotskiy08@gmail.com>
2024-10-24 19:17:01 +0300

    Неважно

--- films/views.py
+++ films/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import ListView
-from django.views.generic import DetailView #---
-from .models import Movie                   #---
+from django.views.generic import DetailView
+from .models import Movie
 from .models import *
 
 
