91d1030583ac89cfd14e741d6e8138ea651072dd
KirillMomotenko <kirill_momotenko@mail.ru>
2024-10-24 18:57:30 +0300

    исправление прошлого коммита

--- films/urls.py
+++ films/urls.py
@@ -7,4 +7,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     #    path('', views.welcome, name='welcome'),
     path('', MovieList.as_view(), name='movie-list'),
+    path('movies/<int:pk>/', MovieDetailView.as_view(), name='movie-detail'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- films/views.py
+++ films/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import ListView
+from django.views.generic import DetailView #---
+from .models import Movie                   #---
 from .models import *
 
 
@@ -12,3 +14,8 @@ class MovieList(ListView):
     template_name = 'films/movies_list.html'
     context_object_name = 'movies'
     paginate_by = 10
+
+class MovieDetailView(DetailView):
+    model = Movie
+    template_name = 'films/movie_detail.html'  # Указываем путь к шаблону
+    context_object_name = 'movie'  # Название переменной для объекта в шаблоне
